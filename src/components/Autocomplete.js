337347517e86bc987dddc8838c23d0483f2176ac
Dali Han <dali4han@gmail.com>
2023-03-16 10:51:02 +0000

    add company logo to autocomplete

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,21 +1,22 @@
 import React, { useState } from 'react';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
-import StockData from './StockData.js';
+import { Box } from '@material-ui/core';
+// import StockData from './StockData.js';
 
 export default function AutocompleteExample() {
   const options = [
-    { value: 'APPL', label: 'APPL - Apple Inc' },
-    { value: 'MSFT', label: 'MSFT - Microsoft Corp' },
-    { value: 'AMZN', label: 'AMZN - Amazon.com, Inc.' },
-    { value: 'TSLA', label: 'TSLA - Tesla Inc' },
-    { value: 'NVDA', label: 'NVDA - NVIDIA Corporation' },
-    { value: 'META', label: 'META - Meta Platforms Inc' },
-    { value: 'GOOG', label: 'GOOG - Alphabet Inc' },
-    { value: 'NFLX', label: 'NFLX - Netflix Inc' },
-    { value: 'AMD', label: 'AMD - Advanced Micro Devices Inc' },
-    { value: 'BABA', label: 'BABA - Alibaba Group Holding Ltd' },
-    { value: 'ADCE', label: 'ADCE - Adobe Systems Incorporated' },
+    { value: 'APPL', label: 'APPL - Apple Inc', logo: 'https://companiesmarketcap.com//img/company-logos/64/AAPL.webp' },
+    { value: 'MSFT', label: 'MSFT - Microsoft Corp', logo: 'https://companiesmarketcap.com//img/company-logos/64/MSFT.webp' },
+    { value: 'AMZN', label: 'AMZN - Amazon.com, Inc.', logo: 'https://companiesmarketcap.com//img/company-logos/64/AMZN.webp' },
+    { value: 'TSLA', label: 'TSLA - Tesla Inc', logo: 'https://companiesmarketcap.com//img/company-logos/64/TSLA.webp' },
+    { value: 'NVDA', label: 'NVDA - NVIDIA Corporation', logo: 'https://companiesmarketcap.com//img/company-logos/64/NVDA.webp' },
+    { value: 'META', label: 'META - Meta Platforms Inc', logo: 'https://companiesmarketcap.com//img/company-logos/64/META.webp' },
+    { value: 'GOOG', label: 'GOOG - Alphabet Inc', logo: 'https://companiesmarketcap.com//img/company-logos/64/GOOG.webp' },
+    { value: 'NFLX', label: 'NFLX - Netflix Inc', logo: 'https://companiesmarketcap.com//img/company-logos/64/NFLX.webp' },
+    { value: 'AMD', label: 'AMD - Advanced Micro Devices Inc', logo: 'https://companiesmarketcap.com//img/company-logos/64/AMD.webp' },
+    { value: 'BABA', label: 'BABA - Alibaba Group Holding Ltd', logo: 'https://companiesmarketcap.com//img/company-logos/64/BABA.webp' },
+    { value: 'ADBE', label: 'ADCE - Adobe Systems Incorporated', logo: 'https://companiesmarketcap.com//img/company-logos/64/ADBE.webp' },
   ];
 
   const [selectedValue, setSelectedValue] = useState(null);
@@ -32,12 +33,27 @@ export default function AutocompleteExample() {
         getOptionSelected={(option, value) => option.value === value.value}
         fullWidth
         onChange={handleChange}
+        renderOption={option => {
+          return (
+              <Box>
+                  <img 
+                    loading="lazy"
+                    width="12"
+                    height="12" 
+                    className="company-logo" 
+                    alt={`${option.value} logo`} 
+                    src={option.logo} 
+                  />
+                    {option.label}
+              </Box>
+          );
+      }}
         renderInput={(params) => (
           <TextField {...params} label="Select an option" variant="outlined" />
         )}
       />
       {selectedValue && <p>You selected: {selectedValue.value}</p>}
-      <StockData selectedValue={selectedValue.value} />
+      {/* <StockData selectedValue={selectedValue.value} /> */}
     </div>
   );
 };

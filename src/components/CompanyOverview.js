68f224cbf9140c2e99bf2f948efcb96534b308f8
Dali Han <ross4han@gmail.com>
2023-03-16 20:19:21 +0000

    populate company overview

--- src/components/CompanyOverview.js
+++ src/components/CompanyOverview.js
@@ -24,17 +24,49 @@ function CompanyOverview({ selectedCompany }) {
   }
   // example json output https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo
   // you can choose what to use
+
+  // function to display big numbers
+  function displayBigMoney(num) {
+    let abbreviations = ["", "thousand", "million", "B", "T"];
   
+    if (num < 1000) {
+      return num.toString();
+    }
+  
+    let i = Math.floor(Math.log10(num) / 3);
+    let val = num / Math.pow(1000, i);
+  
+    return val.toFixed(2) + " " + abbreviations[i];
+  }
+  console.log(overview);
+  const marketCap = displayBigMoney(parseInt(overview['MarketCapitalization']));
+  const grossProfit = displayBigMoney(parseInt(overview['GrossProfitTTM']));
+  const fiftytwoWeekHigh = parseInt(overview['52WeekHigh']);
+  const fiftytwoWeekLow = parseInt(overview['52WeekLow']);
+  // const PERatio = parseInt(overview['PERatio']);
+  // const dividendYield = parseInt(overview['DividendYield']);
+  const currency = overview['Currency'];
+  const exchange = overview['Exchange'];
+  const symbol = overview['Symbol'];
+  const name = overview['Name'];
+  const description = overview['Description'];
+  const sector = overview['Sector'];
+
   return (
     <div>
       <h1>Company Overview Component</h1>
       {selectedCompany && <h2>You selected {selectedCompany}</h2>}
-      <h2>{overview['Symbol']}</h2>
-      <p>Name: {overview['Name']}</p>
-      <p>Desc: {overview['Description']}</p>
-      <p>Exchange: {overview['Exchange']}</p>
-      <p>Sector: {overview['Sector']}</p>
-      <p>Market Capitalization: {overview['MarketCapitalization']}</p>
+      <h2>{symbol}</h2>
+      <p>Name: {name}</p>
+      <p>Description: {description}</p>
+      <p>Exchange: {exchange}</p>
+      <p>Sector: {sector}</p>
+      <p>Market Capitalization: {marketCap} {currency}</p>
+      <p>Gross Proft: {grossProfit} {currency}</p>
+      {/* <p>PERatio: {PERatio}</p> */}
+      <p>Currency: {currency}</p>
+      <p>52-wk high: {fiftytwoWeekHigh}</p>
+      <p>52-wk low: {fiftytwoWeekLow}</p>
     </div>
   );
 }

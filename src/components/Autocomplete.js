083fa0dc7d399e013334029b479c51c478307cb8
Dali Han <dali4han@gmail.com>
2023-03-16 09:29:52 +0000

    connect autocomplete to stockdata

--- src/components/Autocomplete.js
+++ src/components/Autocomplete.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
+import StockData from './StockData.js';
 
 export default function AutocompleteExample() {
   const options = [
     { value: 'APPL', label: 'APPL - Apple Inc' },
-    { value: 'Option 2', label: 'Option 2' },
-    { value: 'Option 3', label: 'Option 3' },
+    { value: 'MSFT', label: 'MSFT - Microsoft Corp' },
+    { value: 'AMZN', label: 'AMZN - Amazon.com, Inc.' },
+    { value: 'TSLA', label: 'TSLA - Tesla Inc' },
+    { value: 'NVDA', label: 'NVDA - NVIDIA Corporation' },
+    { value: 'META', label: 'META - Meta Platforms Inc' },
+    { value: 'GOOG', label: 'GOOG - Alphabet Inc' },
+    { value: 'NFLX', label: 'NFLX - Netflix Inc' },
+    { value: 'AMD', label: 'AMD - Advanced Micro Devices Inc' },
+    { value: 'BABA', label: 'BABA - Alibaba Group Holding Ltd' },
+    { value: 'ADCE', label: 'ADCE - Adobe Systems Incorporated' },
   ];
 
   const [selectedValue, setSelectedValue] = useState(null);
@@ -20,13 +29,15 @@ export default function AutocompleteExample() {
       <Autocomplete
         options={options}
         getOptionLabel={(option) => option.label}
+        getOptionSelected={(option, value) => option.value === value.value}
         fullWidth
         onChange={handleChange}
         renderInput={(params) => (
           <TextField {...params} label="Select an option" variant="outlined" />
         )}
       />
-      {selectedValue && <p>You selected: {selectedValue.label}</p>}
+      {selectedValue && <p>You selected: {selectedValue.value}</p>}
+      <StockData selectedValue={selectedValue.value} />
     </div>
   );
 };

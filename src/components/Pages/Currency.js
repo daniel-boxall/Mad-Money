b7452b09abf75f9fb871d9dc93340b0452306ca9
daniel-boxall <danielboxall07@gmail.com>
2023-03-17 20:31:30 +0000

    router and cureency fixed

--- src/components/Pages/Currency.js
+++ src/components/Pages/Currency.js
@@ -4,7 +4,8 @@ import Button from '../Button';
 
 function Currency() {
     return (
-
+        <>
+        </>
     );
 }
 
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,51 +1,17 @@
 import React from 'react';
-import '../../App.css';
-import CurrencyExchangeRate from '../CurrencyExchangeRate';
-import { Switch } from '@mui/material';
-import { FormGroup } from '@mui/material';
-import { FormControlLabel } from '@mui/material';
 import AutocompleteExample from '../Autocomplete';
+import { Button } from '../Button';
+import { Link } from 'react-router-dom';
 
 function Home() {
     
-    const news = #news;
-    news = [value, setValue] = React.useState(false);
-
-    const handleChange = (e) => {
-        setValue(e.target.checked)
-    }
-
-
+    
     return (
         <>
             <h1>Welcome to Mad Money</h1>
-
-            <FormGroup>
-                <FormControlLabel id="news" label="News" control={
-                    <Switch defaultChecked
-                        checked={value}
-                        onChange={handleChange}
-                        name="news"
-                        iputProps={{ "aria-labe": "test switch"}}
-                />} />
-                <FormControlLabel control={
-                    <Switch
-                        checked={value}
-                        onChange={handleChange}
-                        name="stock"
-                        iputProps={{ "aria-labe": "test switch"}}
-                    />} label="Stock" />
-                <FormControlLabel control={
-                    <Switch
-                        checked={value}
-                        onChange={handleChange}
-                        name="currency"
-                        iputProps={{ "aria-labe": "test switch"}}
-                    />} label="Currency" />
-            </FormGroup>
-            <CurrencyExchangeRate fromCurrency={'GBP'} toCurrency={'CNY'} />
+            <Link to='Currency'><Button buttonStyle='btn--outline'>Currency</Button></Link>
             <AutocompleteExample />
-        </>
+         </>
     );
 }
 

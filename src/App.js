be9cb95ed78b4b9a6f7e063489e69bfa9e290814
daniel-boxall <danielboxall07@gmail.com>
2023-03-17 19:34:35 +0000

    changed file structure, and started toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import './App.css';
-import AutocompleteExample from './components/Autocomplete';
-// import AutocompleteField from './components/Autocomplete';
-// import GithubProfile from './components/GithubProfile';
-// import StockData from './components/StockData.js'
-// // import TickerSearch from './components/TickerSearch';
-// // import SearchBox from './components/SearchBox';
-// import CompanyOverview from './components/CompanyOverview';
-// import CurrencyExchangeRate from './components/CurrencyExchangeRate';
-// import LightweightChart from './components/LightweightChart.js';
-// import AutocompleteExample from './components/Autocomplete';
-import HelloWorld from './components/TestDotEnv';
 import Footer from './components/Footer.js'
 import Home from './components/Pages/Home.js'
 import Team from './components/Pages/Team.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import TickerSearch from './components/TickerSearch';
-// import SearchBox from './components/SearchBox';
-
 
 
 import Navbar from './components/Navbar.js';
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,22 +1,50 @@
 import React from 'react';
 import '../../App.css';
-import StockData from '../StockData';
-import CompanyOverview from '../CompanyOverview';
 import CurrencyExchangeRate from '../CurrencyExchangeRate';
-import LightweightChart from '../LightweightChart';
+import { Switch } from '@mui/material';
+import { FormGroup } from '@mui/material';
+import { FormControlLabel } from '@mui/material';
+import AutocompleteExample from '../Autocomplete';
 
 function Home() {
+    
+    const news = #news;
+    news = [value, setValue] = React.useState(false);
+
+    const handleChange = (e) => {
+        setValue(e.target.checked)
+    }
+
+
     return (
         <>
-            <h1>Hello World!</h1>
-            <HelloWorld />
-            <StockData />
-            <CompanyOverview />
+            <h1>Welcome to Mad Money</h1>
+
+            <FormGroup>
+                <FormControlLabel id="news" label="News" control={
+                    <Switch defaultChecked
+                        checked={value}
+                        onChange={handleChange}
+                        name="news"
+                        iputProps={{ "aria-labe": "test switch"}}
+                />} />
+                <FormControlLabel control={
+                    <Switch
+                        checked={value}
+                        onChange={handleChange}
+                        name="stock"
+                        iputProps={{ "aria-labe": "test switch"}}
+                    />} label="Stock" />
+                <FormControlLabel control={
+                    <Switch
+                        checked={value}
+                        onChange={handleChange}
+                        name="currency"
+                        iputProps={{ "aria-labe": "test switch"}}
+                    />} label="Currency" />
+            </FormGroup>
             <CurrencyExchangeRate fromCurrency={'GBP'} toCurrency={'CNY'} />
-            <LightweightChart />
             <AutocompleteExample />
-      {/* <SearchBox /> */}
-      {/* <TickerSearch /> */}
         </>
     );
 }

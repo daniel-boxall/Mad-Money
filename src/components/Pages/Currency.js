9f2a81b17dcd71df70c5c28440894d383018872c
daniel-boxall <danielboxall07@gmail.com>
2023-03-17 20:27:52 +0000

    new page for currency

---
+++ src/components/Pages/Currency.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import AutocompleteExample from '../Autocomplete';
+import Button from '../Button';
+
+function Currency() {
+    return (
+
+    );
+}
+
+export default Currency

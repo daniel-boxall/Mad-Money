a4f7055754b0faf125d268c20001f4d463b22a9d
Daniel boxall <104602940+daniel-boxall@users.noreply.github.com>
2023-03-19 02:06:02 +0000

    Update Footer.js
    Removed double import

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,6 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import ButtonMailto from '../components/ButtonMailto';
 
 function Footer() {
     return (

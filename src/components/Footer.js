20fa0e48731c2c5d631f9ecd85c67cb329a4e20a
daniel-boxall <danielboxall07@gmail.com>
2023-03-21 19:52:13 +0000

    changed the tab bar asthetics

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,31 +15,31 @@ function Footer() {
                 <div className='row'>
                     <div className='column'>
                         <h2 className='name'>David Jones</h2>
-                        <Link to='https://davejonesuk.dev/'><AccountBoxIcon fontSize='large' /></Link>
+                        <Link target="_blank" to='https://davejonesuk.dev/'><AccountBoxIcon fontSize='large' /></Link>
                         <Link to='https://linkedin.com/in/david-jones-751206257/'><LinkedInIcon fontSize='large' /></Link>
                         <Link to='https://github.com/DJ-86'><GitHubIcon fontSize='large'/></Link>
                         <ButtonMailto label={ <EmailIcon fontSize='large' />} mailto='mailto:davidmarkjones@gmail.com'/>
                     </div>
                     <div className='column'>
                         <h2 className='name'>Daniel Boxall</h2>
-                        <Link to='#'><AccountBoxIcon fontSize='large' /></Link>
+                        <Link target="_blank" to='#'><AccountBoxIcon fontSize='large' /></Link>
                         <Link to='https://linkedin.com/in/daniel-boxall-34a951257'><LinkedInIcon fontSize='large' /></Link>
                         <Link to='https://github.com/daniel-boxall'><GitHubIcon fontSize='large'/></Link>
                         <ButtonMailto label={ <EmailIcon fontSize='large' />} mailto='mailto:danielboxall07@gmail.com'/>
                     </div>
                     <div className='column'>
                         <h2 className='name'>Shah hoque</h2>
-                        <Link to='#'><AccountBoxIcon fontSize='large' /></Link>
+                        <Link target="_blank" to='#'><AccountBoxIcon fontSize='large' /></Link>
                         <Link to='https://linkedin.com/in/'><LinkedInIcon fontSize='large' /></Link>
                         <Link to='https://github.com/shah-hoque'><GitHubIcon fontSize='large'/></Link>
                         <ButtonMailto label={ <EmailIcon fontSize='large' />} mailto='#'/>
                     </div>
                     <div className='column'>
                         <h2 className='name'>Dali Han</h2>
-                        <Link to='#'><AccountBoxIcon fontSize='large' /></Link>
+                        <Link target="_blank" to='#'><AccountBoxIcon fontSize='large' /></Link>
                         <Link to='https://linkedin.com/in/'><LinkedInIcon fontSize='large' /></Link>
                         <Link to='https://github.com/dearbojack'><GitHubIcon fontSize='large'/></Link>
-                        <ButtonMailto label= { <EmailIcon fontSize='large' />} mailto='#'/>
+                        <ButtonMailto label= { <EmailIcon fontSize='large' />} mailto='dali4han@gmail.com'/>
                     </div>
                 </div>
             </div>

fb6d51c950b8ae5b5f44b5634ae345a43ca64d79
Dali Han <dali4han@gmail.com>
2023-03-21 12:33:17 +0000

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 
 import './App.css';
 import AutocompleteExample from './components/Autocomplete';
-// import AutocompleteField from './components/Autocomplete';
-// import GithubProfile from './components/GithubProfile';
-// import StockData from './components/StockData.js'
-// // import TickerSearch from './components/TickerSearch';
-// // import SearchBox from './components/SearchBox';
-// import CompanyOverview from './components/CompanyOverview';
-// import CurrencyExchangeRate from './components/CurrencyExchangeRate';
-// import LightweightChart from './components/LightweightChart.js';
-// import AutocompleteExample from './components/Autocomplete';
-import HelloWorld from './components/TestDotEnv';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <HelloWorld />
       <AutocompleteExample />
     </div>
   );

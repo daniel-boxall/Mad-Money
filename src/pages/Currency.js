ba2081e3fc6e499ccbb81abb69fa1b2e94ba6531
daniel-boxall <danielboxall07@gmail.com>
2023-03-21 22:38:51 +0000

    changed data added footer back in

--- src/pages/Currency.js
+++ src/pages/Currency.js
@@ -7,11 +7,9 @@ import CurrencyProvider from '../components/currencyApp/context/CurrencyContext'
 function Currency() {
     return (
         <>
-            <div className='main' style={{ "height": "70.51vh" }}>
-                <CurrencyProvider>
-                    <CurrencyConverter />
-                </CurrencyProvider>
-            </div>  
+            <CurrencyProvider>
+                <CurrencyConverter />
+            </CurrencyProvider>
         </>
     );
 }
